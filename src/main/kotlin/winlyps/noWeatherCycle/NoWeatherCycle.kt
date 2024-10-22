db72d24fae0cea4100ffa6faa5fe72989beca259
Winlypse <nts3@o2.pl>
2024-10-22 15:27:02 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noWeatherCycle/NoWeatherCycle.kt
@@ -0,0 +1,14 @@
+package winlyps.noWeatherCycle
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoWeatherCycle : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
