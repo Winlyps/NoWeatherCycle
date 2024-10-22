51c0ad142ab9e381ee3526027a3fb90e0095e3ed
Winlypse <nts3@o2.pl>
2024-10-22 15:27:14 +0200

    Changes!

--- src/main/kotlin/winlyps/noWeatherCycle/NoWeatherCycle.kt
+++ src/main/kotlin/winlyps/noWeatherCycle/NoWeatherCycle.kt
@@ -6,9 +6,14 @@ class NoWeatherCycle : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("doWeatherCycle", "false")
+        }
+        logger.info("NoWeatherCycle plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoWeatherCycle plugin has been disabled.")
     }
 }
